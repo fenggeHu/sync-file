91ddbd388df2db995881b90e7f93348cb3c8cd11
hujinfeng <pbfoxhu@gmail.com>
2021-11-18 09:19:41 +0800

    win启动脚本失败

--- run.bat
+++ run.bat
@@ -1,10 +1,2 @@
 @echo off
-
-if "%1"=="h" goto begin
-
-start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
-
-:begin
-
-java -server -Xms128M -Xmx1G -jar D:\.m2\repository\hu\jinfeng\sync-file\1.0-SNAPSHOT\sync-file-1.0-SNAPSHOT.jar server adblocation=D:\syncfile
-pause
+// TODO 前面写的脚本失败了
